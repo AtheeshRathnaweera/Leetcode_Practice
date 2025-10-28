a0c915417d5d6439458dc5ea77a936d53476fc46
AtheeshRathnaweera <rathnaweeraatheesh72@gmail.com>
2025-10-28 19:47:52 +0530

    pre-order and post-order traversal of binary trees

--- Practice/trees.py
+++ Practice/trees.py
@@ -10,8 +10,8 @@
 # 6. AVL Trees
 # 7. B-Trees
 ###################################################################################
-# REPRESENTATION
 # Binary Tree
+# REPRESENTATION
 class BTreeNode:
     def __init__(self, val, left, right) -> None:
         self.val = val
@@ -36,7 +36,9 @@ binary_tree = BTreeNode(
     BTreeNode(9, None, BTreeNode(9, BTreeNode(5, None, None), None)),
 )
 
+# TRAVERSAL
 # In-order Traversal
+# left, root, right
 def in_order_traversal(node:BTreeNode):
     if node:
         in_order_traversal(node.left)
@@ -45,3 +47,42 @@ def in_order_traversal(node:BTreeNode):
 
 in_order_traversal(binary_tree)
 # Expected: [2, 7, 5, 6, 11, 1, 9, 5, 9]
+
+print()
+
+# Pre-order Traversal
+# root, left, right
+def pre_order_traversal(node:BTreeNode):
+    if node:
+        print(node.val, end=" ")
+        pre_order_traversal(node.left)
+        pre_order_traversal(node.right)
+
+pre_order_traversal(binary_tree)
+# Expected: [1, 7, 2, 6, 5, 11, 9, 9, 5]
+
+print()
+
+# Post-order Traversal
+# left, right, root
+def post_order_traversal(node:BTreeNode):
+    if node:
+        post_order_traversal(node.left)
+        post_order_traversal(node.right)
+        print(node.val, end=" ")
+
+post_order_traversal(binary_tree)
+# Expected: [2, 5, 11, 6, 7, 5, 9, 9, 1]
+
+###################################################################################
+# Full Binary Tree
+
+
+###################################################################################
+# Binary Search Tree
+
+###################################################################################
+# AVL Tree
+
+###################################################################################
+# B - Trees
